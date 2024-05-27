fd39bd2461ffc06f772ea730faf9d408ce1b71c9
Geng Luo <geng.luo@student.kuleuven.be>
2024-05-27 12:11:00 +0200

    Change the filebox url back to original url

--- download.sh
+++ download.sh
@@ -3,11 +3,16 @@ fb_status=$(wget --spider -S https://filebox.ece.vt.edu/ 2>&1 | grep  "HTTP/1.1 
 
 mkdir checkpoints
 
-echo "downloading from filebox ..."
-wget https://filebox.ece.vt.edu/~jbhuang/project/3DPhoto/model/color-model.pth
-wget https://filebox.ece.vt.edu/~jbhuang/project/3DPhoto/model/depth-model.pth
-wget https://filebox.ece.vt.edu/~jbhuang/project/3DPhoto/model/edge-model.pth
-wget https://filebox.ece.vt.edu/~jbhuang/project/3DPhoto/model/model.pt
+# echo "downloading from filebox ..."
+# wget https://filebox.ece.vt.edu/~jbhuang/project/3DPhoto/model/color-model.pth
+# wget https://filebox.ece.vt.edu/~jbhuang/project/3DPhoto/model/depth-model.pth
+# wget https://filebox.ece.vt.edu/~jbhuang/project/3DPhoto/model/edge-model.pth
+# wget https://filebox.ece.vt.edu/~jbhuang/project/3DPhoto/model/model.pt
+echo "downloading from other link ..."
+wget https://github.com/LemonATsu/3d-photo-model-weights/raw/master/color-model.pth
+wget https://github.com/LemonATsu/3d-photo-model-weights/raw/master/depth-model.pth
+wget https://github.com/LemonATsu/3d-photo-model-weights/raw/master/edge-model.pth
+wget https://github.com/LemonATsu/3d-photo-model-weights/raw/master/model.pt
 
 mv color-model.pth checkpoints/.
 mv depth-model.pth checkpoints/.
@@ -19,7 +24,8 @@ git clone https://github.com/compphoto/BoostingMonocularDepth.git
 mkdir -p BoostingMonocularDepth/pix2pix/checkpoints/mergemodel/
 
 echo "downloading mergenet weights ..."
-wget https://filebox.ece.vt.edu/~jbhuang/project/3DPhoto/model/latest_net_G.pth
+wget https://sfu.ca/~yagiz/CVPR21/latest_net_G.pth
+# wget https://filebox.ece.vt.edu/~jbhuang/project/3DPhoto/model/latest_net_G.pth
 mv latest_net_G.pth BoostingMonocularDepth/pix2pix/checkpoints/mergemodel/
 wget https://github.com/intel-isl/MiDaS/releases/download/v2/model-f46da743.pt
 mv model-f46da743.pt BoostingMonocularDepth/midas/model.pt
